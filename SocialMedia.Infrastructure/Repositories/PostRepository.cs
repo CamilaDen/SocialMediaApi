7e3744b49248faa2d084afc8a1aa0b091a8d19bb
Camila Denise Goncalves <86990957+CamilaDen@users.noreply.github.com>
2023-03-30 16:40:26 -0300

    Agregar archivos de proyecto.

---
+++ SocialMedia.Infrastructure/Repositories/PostRepository.cs
@@ -0,0 +1,26 @@
+﻿using SocialMedia.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SocialMedia.Infrastructure.Repositories
+{
+    public class PostRepository
+    {
+        public IEnumerable<Post> GetPosts()
+        {
+            //Range simula un ciclo de 1 a 10 y lo utilizo para cargar y listar las propiedades
+            var posts = Enumerable.Range(1, 10).Select(x => new Post
+            {
+                PostId = x,
+                Description = $"Description {x}",
+                Date = DateTime.Now,
+                Image = $"https://misapis.com/{x}",
+                UserId = x * 2
+            }) ;
+
+            return posts;
+        }
+    }
+}
